6b223ca16d5eaf7c3a2ecebd1e3867da5442d538
Jan <j.peix04@gmail.com>
2026-01-21 15:12:55 +0100

    Router implmented

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from app.api.main import router
 
 app = FastAPI(title="SC-Kura", description="SC-Kura API", version="0.0.1")
+app.include_router(router)
 
 
 @app.get("/")
