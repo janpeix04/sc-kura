45e860e2f2a134008c720e06440273a6c1f6a416
Jan <j.peix04@gmail.com>
2026-01-21 16:34:10 +0100

    Health test

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,6 @@ app = FastAPI(title="SC-Kura", description="SC-Kura API", version="0.0.1")
 app.include_router(router)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/health")
+def health():
+    return {"status": "ok"}
