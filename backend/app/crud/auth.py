7c35c2c4f4b1d8edce0d5f2ca3e086952454bb75
Jan <j.peix04@gmail.com>
2026-02-06 15:36:39 +0100

    User me endpoints

--- backend/app/crud/auth.py
+++ backend/app/crud/auth.py
@@ -1,7 +1,7 @@
-from sqlmodel import select
+from sqlmodel import select, func
 from sqlmodel.ext.asyncio.session import AsyncSession
 from app.models.users import User
-from app.schemas.users import UserCreate
+from app.schemas.users import UserCreate, UserUpdate
 from app.core.security import get_password_hash, verify_password
 
 
@@ -39,3 +39,65 @@ async def get_user_by_id(session: AsyncSession, id: str) -> User | None:
     stmt = select(User).where(User.id == id)
     result = await session.exec(stmt)
     return result.first()
+
+
+async def verify_user(*, session: AsyncSession, db_user: User) -> User:
+    db_user.is_verified = True
+    session.add(db_user)
+    await session.commit()
+    await session.refresh(db_user)
+    return db_user
+
+
+async def update_user(
+    *, session: AsyncSession, db_user: User, user_in: UserUpdate
+) -> User:
+    user_data = user_in.model_dump(exclude_unset=True)
+    extra_data = {}
+    if "password" in user_data:
+        password = user_data["password"]
+        hashed_password = get_password_hash(password)
+        extra_data["hashed_password"] = hashed_password
+    db_user.sqlmodel_update(user_data, update=extra_data)
+    session.add(db_user)
+    await session.commit()
+    await session.refresh(db_user)
+    return db_user
+
+
+async def update_user_me(
+    *, session: AsyncSession, db_user: User, user_in: UserUpdate
+) -> User:
+    user_data = user_in.model_dump(exclude_unset=True)
+    db_user.sqlmodel_update(user_data)
+    session.add(db_user)
+    await session.commit()
+    await session.refresh(db_user)
+    return db_user
+
+
+async def update_password_me(*, session: AsyncSession, db_user: User) -> None:
+    session.add(db_user)
+    await session.commit()
+    return
+
+
+async def delete_user(*, session: AsyncSession, db_user: User) -> None:
+    db_user = await session.get(User, db_user.id)
+    await session.delete(db_user)
+    await session.commit()
+    return
+
+
+async def get_users(
+    *, session: AsyncSession, skip: int = 0, limit: int | None = None
+) -> tuple[list[User], int]:
+    count_stmt = select(func.count()).select_from(User)
+    count = await session.exec(count_stmt)
+
+    stmt = select(User)
+    if limit is not None:
+        stmt = stmt.offset(skip).limit(limit)
+    users = await session.exec(stmt)
+
+    return users.all(), count.one()
