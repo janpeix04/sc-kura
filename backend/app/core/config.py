123f7c57866b0d01a3cc2fd7c746398392cb2dca
Jan <j.peix04@gmail.com>
2026-02-06 23:52:10 +0100

    Fixing ci

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -39,12 +39,12 @@ class Settings(BaseSettings):
     MAIL_USERNAME: str
     MAIL_FROM: str
     MAIL_PASSWORD: str
-    MAIL_PORT: int
-    MAIL_SERVER: str
-    MAIL_STARTTLS: bool
-    MAIL_SSL_TLS: bool
-    USE_CREDENTIALS: bool
-    VALIDATE_CERTS: bool
+    MAIL_PORT: int = 587
+    MAIL_SERVER: str = "smtp.gmail.com"
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
+    USE_CREDENTIALS: bool = True
+    VALIDATE_CERTS: bool = True
 
     @property
     def DATABASE_URL(self) -> str:
