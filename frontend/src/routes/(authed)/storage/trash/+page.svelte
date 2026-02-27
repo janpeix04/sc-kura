f7f5f739c8eaeb5bb1336a46bc141e9f5f492502
Jan <j.peix04@gmail.com>
2026-02-27 15:57:53 +0100

    Trash page with empty message

---
+++ frontend/src/routes/(authed)/storage/trash/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<div class="bg-tertiary-foreground flex h-full w-full">
+	<main class="flex flex-1 flex-col gap-4 p-6">
+		<div class="bg-background flex min-h-0 flex-1 flex-col rounded-lg p-4">
+			<div class="py-2">
+				<h2 class="text-lg font-semibold">Trash</h2>
+			</div>
+
+			<div class="flex h-full flex-col items-center justify-center gap-2">
+				<span class="icon-[mdi--trash-can-empty] size-32 bg-gray-300"></span>
+				<span class="text-xl font-semibold">Trash is empty</span>
+				<span>Items moved to the recycle bin will be deleted forever after 30 days</span>
+			</div>
+		</div>
+	</main>
+</div>
