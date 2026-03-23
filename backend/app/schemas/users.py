71c866141f3d249a25a9aaaace72adbaa8fb6c84
Jan <j.peix04@gmail.com>
2026-03-23 20:59:06 +0100

    User Models

--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -5,6 +5,17 @@ from pydantic import EmailStr
 class UserBase(SQLModel):
     first_name: str = Field(nullable=False, min_length=2, max_length=50)
     last_name: str = Field(nullable=False, min_length=2, max_length=50)
-    emial: EmailStr = Field(nullable=False, unique=True, max_length=255)
+    email: EmailStr = Field(nullable=False, unique=True, max_length=255)
     is_verified: bool = Field(default=False)
     is_superuser: bool = Field(default=False)
+
+
+class UserCreate(UserBase):
+    password: str = Field(nullable=False, min_length=8)
+
+
+class UserRegister(SQLModel):
+    first_name: str = Field(nullable=False, min_length=2, max_length=50)
+    last_name: str = Field(nullable=False, min_length=2, max_length=50)
+    email: EmailStr = Field(nullable=False, unique=True, max_length=255)
+    password: str = Field(nullable=False, min_length=8)
