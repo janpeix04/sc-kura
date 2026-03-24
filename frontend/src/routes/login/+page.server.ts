7f5936308eeddd6d4c2b34777679bbae6fc49f8e
Jan <j.peix04@gmail.com>
2026-03-24 22:22:19 +0100

    Handle access_token

--- frontend/src/routes/login/+page.server.ts
+++ frontend/src/routes/login/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from './$types';
 import { fail, message, superValidate } from 'sveltekit-superforms';
 import { zod4 } from 'sveltekit-superforms/adapters';
 import { loginSchema } from '$lib/schemas/auth';
-import { loginPost } from '$lib/client';
+import { loginPost, usersMeGet } from '$lib/client';
 import { localizeHref } from '$lib/paraglide/runtime';
 import { m } from '$lib/paraglide/messages';
 
@@ -26,7 +26,7 @@ export const load: PageServerLoad = async ({ url }) => {
 };
 
 export const actions: Actions = {
-	login: async ({ request }) => {
+	login: async ({ request, cookies }) => {
 		const form = await superValidate(request, zod4(loginSchema));
 
 		if (!form.valid) {
@@ -41,8 +41,27 @@ export const actions: Actions = {
 		});
 
 		if (!error) {
-			return redirect(303, localizeHref(`/signup`));
+			const { access_token } = data;
+			cookies.set('access_token', access_token, {
+				httpOnly: true,
+				secure: true,
+				sameSite: 'lax',
+				path: '/',
+				maxAge: 60 * 60
+			});
+
+			const { data: user, response } = await usersMeGet({
+				headers: {
+					Authorization: `Bearer ${access_token}`
+				}
+			});
+
+			if (response.ok && user) {
+				redirect(303, localizeHref(`/home`));
+			}
 		}
+
+		if (error === undefined) return message(form, m.oops_something_went_wrong(), { status: 500 });
 
 		if ('msg' in error) {
 			return message(form, error.msg, { status: 400 });
