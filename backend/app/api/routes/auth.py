7a2dd9de5678a388a98df80cbdde96b8b0b73450
Jan <j.peix04@gmail.com>
2026-03-24 12:12:11 +0100

    Validate user before creating

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -1,14 +1,12 @@
-from typing import Annotated
-from fastapi import APIRouter, Form
+from fastapi import APIRouter
 
-from app.deps.auth import SessionDep
-from app.schemas.users import UserRegister
+from app.deps.auth import SessionDep, ValidatedUserRegister
 from app.crud import auth as auth_crud
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @router.post("/signup/", response_model=str)
-async def sign_up(session: SessionDep, user_create: Annotated[UserRegister, Form()]):
+async def sign_up(session: SessionDep, user_create: ValidatedUserRegister):
     await auth_crud.create_user(session=session, user_create=user_create)
     return "User registered successfully"
