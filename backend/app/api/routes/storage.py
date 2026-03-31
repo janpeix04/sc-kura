208e393f90bb565f26b6c6520de053fd217dd4b9
Jan <j.peix04@gmail.com>
2026-03-31 16:39:13 +0200

    Create and get root folder

--- backend/app/api/routes/storage.py
+++ backend/app/api/routes/storage.py
@@ -49,3 +49,35 @@ async def get_folders_in_folder(
         session=session, parent_id=folder_in.id
     )
     return folders
+
+
+@router.get("/folder/root/", response_model=FolderPublic, responses=add_responses(404))
+async def get_root_folder(
+    session: SessionDep, current_user: CurrentUser
+) -> FolderPublic:
+    root = await storage_crud.get_root_folder(session=session, user_id=current_user.id)
+    if not root:
+        raise HTTPError(404, _("Root folder not found"))
+    return FolderPublic(
+        id=root.id,
+        name=root.name,
+        path=root.path,
+        type=root.type,
+        size=root.size,
+        owner=f"{current_user.first_name} {current_user.last_name}",
+        modified_at=root.modified_at,
+        opened_at=root.opened_at,
+        created_at=root.created_at,
+    )
+
+
+@router.post("/folder/root/", response_model=str)
+async def create_root_folder(session: SessionDep, current_user: CurrentUser) -> str:
+    folder_create = FolderCreate(
+        name="/",
+        path="/",
+        owner=f"{current_user.first_name} {current_user.last_name}",
+        user_id=current_user.id,
+    )
+    await storage_crud.create_folder(session=session, folder_create=folder_create)
+    return _("Root folder created successfully")
