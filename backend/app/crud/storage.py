b95903347c0ee76fbe309683ba986f9d2e168081
Jan <j.peix04@gmail.com>
2026-03-31 16:39:13 +0200

    Create and get root folder
    Crud operations

--- backend/app/crud/storage.py
+++ backend/app/crud/storage.py
@@ -3,6 +3,7 @@ import uuid
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 from app.models import Folder
+from app.schemas.storage import FolderCreate
 
 
 async def get_folder_by_id(
@@ -19,3 +20,21 @@ async def get_folders_in_folders(
     stmt = select(Folder).where(Folder.parent_id == parent_id)
     results = await session.exec(stmt)
     return results.all()
+
+
+async def get_root_folder(
+    *, session: AsyncSession, user_id: uuid.UUID
+) -> Folder | None:
+    stmt = select(Folder).where((Folder.path == "/") & (Folder.user_id == user_id))
+    result = await session.exec(stmt)
+    return result.first()
+
+
+async def create_folder(
+    *, session: AsyncSession, folder_create: FolderCreate
+) -> Folder:
+    folder = Folder.model_validate(folder_create)
+    session.add(folder)
+    await session.commit()
+    await session.refresh(folder)
+    return folder
