b1a4d240702cdcb6d64bd951e57449163a8d2b8c
Jan <j.peix04@gmail.com>
2026-04-07 15:35:41 +0200

    New action button

--- frontend/src/lib/layouts/StorageLayout.svelte
+++ frontend/src/lib/layouts/StorageLayout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 	import type { UserPublic } from '$lib/client';
 	import Nav from '$lib/components/Nav.svelte';
-	import Button from '$lib/components/ui/button/button.svelte';
 	import type { Snippet } from 'svelte';
+	import { Button } from '$lib/components/ui/button';
+	import { m } from '$lib/paraglide/messages';
 
 	let {
 		user,
@@ -17,10 +19,29 @@
 	<Nav {user} />
 	<div class="flex flex-1 overflow-hidden">
 		<aside class="w-72 shrink-0 px-4 py-4">
-			<Button>
-				<span class="icon-[lucide--plus] size-4"></span>
-				New
-			</Button>
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger>
+					<Button>
+						<span class="icon-[lucide--plus] size-4"></span>
+						{m.new()}
+					</Button>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content class="w-fit">
+					<DropdownMenu.Item class="cursor-pointer">
+						<span class="icon-[lucide--folder-plus] size-4"></span>
+						{m.new_folder()}
+					</DropdownMenu.Item>
+					<DropdownMenu.Separator />
+					<DropdownMenu.Item class="cursor-pointer">
+						<span class="icon-[lucide--file-plus] size-4"></span>
+						{m.file_upload()}
+					</DropdownMenu.Item>
+					<DropdownMenu.Item class="cursor-pointer">
+						<span class="icon-[lucide--folder-up] size-4"></span>
+						{m.folder_upload()}
+					</DropdownMenu.Item>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
 		</aside>
 
 		<main class="flex-1 overflow-auto pr-4 pb-6">
