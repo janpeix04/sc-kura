cac9a1fe10218d8f4bbdccf68b571005c5c8a94e
Jan <j.peix04@gmail.com>
2026-04-08 21:49:41 +0200

    Prints removed

--- backend/app/crud/storage.py
+++ backend/app/crud/storage.py
@@ -20,7 +20,6 @@ async def get_folder_by_id(
 async def get_folders_in_folders(
     *, session: AsyncSession, parent_id: uuid.UUID
 ) -> list[Folder]:
-    print(parent_id)
     stmt = select(Folder).where(Folder.parent_id == parent_id)
     results = await session.exec(stmt)
     return results.all()
