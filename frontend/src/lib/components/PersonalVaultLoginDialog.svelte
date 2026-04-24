4ce681c2505a4e5e584db22cfc6c43499ffbf4d5
Jan <j.peix04@gmail.com>
2026-04-24 19:39:14 +0200

    Create and get user keys

---
+++ frontend/src/lib/components/PersonalVaultLoginDialog.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import * as Dialog from '$lib/components/ui/dialog';
+	import * as Form from '$lib/components/ui/form';
+	import Input from './ui/input/input.svelte';
+
+	let {
+		open = $bindable()
+	}: {
+		open: boolean;
+	} = $props();
+</script>
+
+<Dialog.Root bind:open>
+	<Dialog.Content
+		showCloseButton={false}
+		onInteractOutside={(e) => e.preventDefault()}
+		onEscapeKeydown={(e) => e.preventDefault()}
+	>
+		<form action="">
+			<Form.Field>
+				<Form.Control>
+					{#snippet children({ props })}
+						<Input
+							type="password"
+							placeholder="••••••••"
+							autocomplete="new-password"
+							bind:value={password}
+							required
+						/>
+					{/snippet}
+				</Form.Control>
+			</Form.Field>
+		</form>
+	</Dialog.Content>
+</Dialog.Root>
