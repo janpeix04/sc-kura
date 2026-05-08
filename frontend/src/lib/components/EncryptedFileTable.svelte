034a5f4ad39b0ecaa2b9593f71710d88df880889
Jan <j.peix04@gmail.com>
2026-05-08 17:08:22 +0200

    Show action button icon

--- frontend/src/lib/components/EncryptedFileTable.svelte
+++ frontend/src/lib/components/EncryptedFileTable.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import type { EncryptedFilePublic, FilePublic, FolderPublic } from '$lib/client';
 	import { m } from '$lib/paraglide/messages';
 	import type { DecryptedFile, DecryptedFolder, Mode, SortKeys } from '$lib/schemas/types';
 	import { formatBytes, formatDate } from '$lib/utilities/utils';
-	import ActionsButton from './ActionsButton.svelte';
 	import EncryptedActionsButton from './EncryptedActionsButton.svelte';
 
 	let {
@@ -55,7 +53,7 @@
 	}
 
 	function applySort() {
-		/* if (files) files = [...files].sort(compare); */
+		if (files) files = [...files].sort(compare);
 		if (folders) folders = [...folders].sort(compare);
 	}
 
@@ -182,7 +180,7 @@
 					<td class="px-4 py-3 text-sm">{formatBytes(file.size)}</td>
 
 					<td class="flex justify-end px-4 py-3">
-						<!-- <ActionsButton item={file} {mode} /> -->
+						<EncryptedActionsButton item={file} {location} />
 					</td>
 				</tr>
 			{/each}
